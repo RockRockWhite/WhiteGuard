99855201c58d90f8dedc1fe00a5167f1d95593a7
RockRockWhite <1834309151@qq.com>
2021-02-03 22:46:53 +0800

    readme

--- server/WebServer.py
+++ server/WebServer.py
@@ -1,8 +1,7 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template
 from pyecharts.charts import Line
 from pyecharts import options as opts
 from jinja2 import Markup
-
 
 app = Flask(__name__)
 
@@ -24,13 +23,24 @@ class WebServer:
         return flow_list
 
 
-@app.route("/")
+@app.route("/233")
 def hello():
     xaxis = []
     for i in range(60):
         xaxis.append(str(-i))
     line = Line().add_xaxis(xaxis[::-1]).add_yaxis("主机1", flow_list)
     return str(Markup(line.render_embed()))
+
+
+@app.route("/")
+def index():
+    xaxis = []
+    for i in range(60):
+        xaxis.append(str(-i))
+    line = Line().add_xaxis(xaxis[::-1]).add_yaxis("主机1", flow_list).set_global_opts(
+        title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="AB数据对比"))
+
+    return render_template("index.html", in_options=line.dump_options(), out_options=line.dump_options())
 
 
 def main():
